c61f056476e4a4963bf474bc07355002d9c02e44
Luqman <luqmanhaqeemsamsul@gmail.com>
2021-02-18 15:58:03 +0800

    create new list

---
+++ resources/views/todo/create.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Add list</div>
+
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                      
+                        <form action="{{route('todo.store')}}" method="POST">
+                            @csrf
+                            <div class="form-group">
+                                <label for="listname">Name</label>
+                                <input type="text" class="form-control" id="listname" name="listname" placeholder="Enter list name">
+                            </div>
+                            <button type="submit" class="btn btn-primary">Add</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
