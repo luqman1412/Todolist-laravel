b4348608598ebdccfe7b411454a617f5985a1e9f
Luqman <luqmanhaqeemsamsul@gmail.com>
2021-03-04 23:01:44 +0800

    update todo model

--- app/todolist.php
+++ app/todolist.php
@@ -1,14 +1,16 @@
 <?php
 
 namespace App;
-
 use Illuminate\Database\Eloquent\Model;
 
 class todolist extends Model
 {
     //
     protected $table = 'todolists';
-    protected $fillable = [
-        'name', 
-    ];
+    protected $fillable = ['name', 'label_id'];
+
+    public function label()
+    {
+        return $this->belongsTo(Labels::class,'label_id');
+    }
 }
